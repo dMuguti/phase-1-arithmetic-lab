96af6c1309af2253389f99528db9912b9049ecec
dMuguti <omiahderek56@gmail.com>
2023-03-21 08:05:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+function randomInt() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+  const random = randomInt();
+  
+
+  const num3 = 14;
+  const num4 = 5;
+  const mod = num3 % num4;
+  
+  function max(...nums) {
+    return Math.max(...nums);
+  }
+
+  
+
+
+  
+  
